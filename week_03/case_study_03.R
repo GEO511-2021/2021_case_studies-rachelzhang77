d35e7441a957f0936630e6d8c49a772208c5025a
Rachel Zhang <zzhang67@buffalo.edu>
2021-09-21 15:48:55 -0400

    the first version

--- week_03/case_study_03.R
+++ week_03/case_study_03.R
@@ -1,1 +1,46 @@
- 
+install.packages("gapminder")
+
+library(ggplot2)
+library(gapminder)
+library(dplyr)
+
+##1
+gapminder_=filter(gapminder,country!="Kuwait")
+gapminder_
+
+##2
+ggplot(gapminder_,aes(x=lifeExp, y=gdpPercap,color=continent,size=pop/100000))+
+  geom_point()+
+  facet_wrap(~year,nrow=1)+
+  scale_y_continuous(trans = "sqrt")+
+  theme_bw()+
+  labs(title="Wealth and life expectancy through time",x="Life Expectancy",y="GDP per capita")
+
+gapminder_
+
+ggsave("task 3_1.png",width=15)
+
+##3
+gapminder_continent=gapminder_ %>%
+  group_by(continent,year) %>%
+  summarize(gdpPercapweighted = weighted.mean(x = gdpPercap, w = pop),
+            pop = sum(as.numeric(pop)))
+
+gapminder_continent
+##4
+gapminder
+ggplot(gapminder_,aes(x=year, y=gdpPercap, color=continent,size=pop/100000)) + 
+  geom_line(aes(size=0.01)) + 
+  geom_point() + 
+  geom_line(data=gapminder_continent,aes(x=year,y=gdpPercapweighted),color="black",size=0.5) + 
+  geom_point(data=gapminder_continent,aes(x=year,y=gdpPercapweighted),color="black") + 
+  facet_wrap(~continent,nrow=1) + 
+  theme_bw() + 
+  labs(x="Year",y="GDP per capita",caption="Population(100k)")
+
+##5
+ggsave("task 3_2.png",width=15)
+
+
+
+
