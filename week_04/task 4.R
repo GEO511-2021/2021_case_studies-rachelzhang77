e602e3dd8047deca85c9207a0264297edfe60534
Rachel Zhang <zzhang67@buffalo.edu>
2021-09-28 10:44:57 -0400

    2nd try

--- week_04/task 4.R
+++ week_04/task 4.R
@@ -10,10 +10,21 @@ str(flights)
 arrange(.data = flights,... = distance)
 slice(.data = flights,... =  distance)
 
+##join two datasets together
 view(airports)
 view(flights)
 join2=left_join(flights,airports,by=c("dest"="faa"))
 
-view(join2)
 
-select(join2,)
+##keep the only airport that is furthest from the airport
+view(join2)
+farthest_airport=arrange(join2,desc(distance))%>%
+  select(name)%>%
+slice(1)%>%
+  as.character()
+
+
+str(farthest_airport)
+
+
+select(join2,"name")
