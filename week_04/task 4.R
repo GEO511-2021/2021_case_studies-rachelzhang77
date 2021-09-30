742be7d2a9912befab40cc6ce063bf6cb7d1faf6
Rachel Zhang <zzhang67@buffalo.edu>
2021-09-29 20:45:41 -0400

    with comments

--- week_04/task 4.R
+++ week_04/task 4.R
@@ -6,25 +6,23 @@ library(nycflights13)
 
 str(flights)
 
-##arrange according to the distance
-arrange(.data = flights,... = distance)
-slice(.data = flights,... =  distance)
 
-##join two datasets together
+##see the structure of two datasets
 view(airports)
 view(flights)
+
+##join two datasets together in order to get the full name of the airports
 join2=left_join(flights,airports,by=c("dest"="faa"))
 
 
 ##keep the only airport that is furthest from the airport
 view(join2)
+## rearrange the combined dataset by using the furthest distance
 farthest_airport=arrange(join2,desc(distance))%>%
   select(name)%>%
-slice(1)%>%
-  as.character()
-
+slice(1)%>% ##only keep the airport value which is furthest from the nyc airport
+  as.character() ##save it as an object
 
 str(farthest_airport)
 
 
-select(join2,"name")
